0e5b4757cc45c54811cd4758e86eb56ee3b64144
wildan <wildan.gery90@gmail.com>
2024-03-27 11:46:27 +0700

    initial commit

---
+++ Finance.Expensia.Web/Areas/Account/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rapid.Areas.Account.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
