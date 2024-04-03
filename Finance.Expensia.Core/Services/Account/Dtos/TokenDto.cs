8b74f6b217f8727264997237b3d72bb2f35da221
wisnu.adhipradana <wa.pradana01@gmail.com>
2024-04-03 07:53:26 +0700

    Add token service

---
+++ Finance.Expensia.Core/Services/Account/Dtos/TokenDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Finance.Expensia.Core.Services.Account.Dtos
+{
+    public class TokenDto
+    {
+        public Guid UserId { get; set; }
+        public required string AccessToken { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public required string RefreshToken { get; set; }
+        public DateTime SessionExpiredAt { get; set; }
+    }
+}
