9daeb6e3d07e4e4769dd63bdb07f544a414ba4ad
wisnu.adhipradana <wa.pradana01@gmail.com>
2024-04-08 14:38:39 +0700

    Add table outgoing payment

---
+++ Finance.Expensia.DataAccess/Builders/OutgoingPaymentEntityBuilder.cs
@@ -0,0 +1,67 @@
+﻿using Finance.Expensia.DataAccess.Bases;
+using Finance.Expensia.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Expensia.DataAccess.Builders
+{
+    public class OutgoingPaymentEntityBuilder : EntityBaseBuilder<OutgoingPayment>
+    {
+        public override void Configure(EntityTypeBuilder<OutgoingPayment> builder)
+        {
+            base.Configure(builder);
+
+            builder
+                .Property(e => e.TransactionNo)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.Requestor)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.CompanyName)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.ApprovalStatus)
+                .HasConversion<string>()
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.ExpectedTransfer)
+                .HasConversion<string>()
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.Remark)
+                .HasMaxLength(200);
+
+
+            builder
+                .Property(e => e.FromBankAliasName)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.FromAccountNo)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.ToBankAliasName)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.ToAccountNo)
+                .HasMaxLength(200);
+
+            builder
+                .Property(e => e.TotalAmount)
+                .HasColumnType("money");
+        }
+    }
+}
---
+++ Finance.Expensia.DataAccess/Models/OutgoingPaymentDetail.cs
@@ -0,0 +1,21 @@
+﻿using Finance.Expensia.DataAccess.Bases;
+
+namespace Finance.Expensia.DataAccess.Models
+{
+    public class OutgoingPaymentDetail : EntityBase
+    {
+        public Guid OutgoingPaymentId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public Guid PartnerId { get; set; }
+        public string PartnerName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Guid ChartOfAccountid { get; set; }
+        public string ChartOfAccountNo { get; set; } = string.Empty;
+        public Guid CostCenterId { get; set; }
+        public string CostCenterName { get; set;} = string.Empty;
+        public decimal Amount { get; set; }
+
+        public virtual OutgoingPayment OutgoingPayment { get; set; } = null!;
+        public virtual List<OutgoingPaymentDetailAttachment>? OutgointPaymentDetailAttachments { get; set; }
+    }
+}
