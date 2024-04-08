00c29b03a538db29eb48a8d552eb6fa5e1e8dba8
wildan <wildan.gery90@gmail.com>
2024-04-08 15:46:36 +0700

    add api login

--- Finance.Expensia.Web/Areas/Core/Controllers/FinanceController.cs
+++ Finance.Expensia.Web/Areas/Core/Controllers/FinanceController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Finance.Expensia.Web.Areas.Core.Controllers
 {
     public class FinanceController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public IActionResult Create()
         {
             return View();
