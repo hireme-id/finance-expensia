4640ef1651ba62e59839f674dc951abfd9132a49
wildan <wildan.gery90@gmail.com>
2024-04-20 11:05:35 +0700

    penambahan controller mailbox dan frontendnya

---
+++ Finance.Expensia.Web/Areas/Core/Controllers/MailboxController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Expensia.Web.Areas.Core.Controllers
+{
+	public class MailboxController : Controller
+	{
+		[AllowAnonymous]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        [AllowAnonymous]
+        public IActionResult Approval()
+        {
+            return View();
+        }
+    }
+}
