d0c8b39ee5baef0ec92f4ce740085f815df0af01
ronnyharianto <v.ronny.harianto@gmail.com>
2024-05-14 08:00:11 +0700

    fix formatting code

--- Finance.Expensia.Core/Services/Inbox/InboxService.cs
+++ Finance.Expensia.Core/Services/Inbox/InboxService.cs
@@ -32,10 +32,17 @@ namespace Finance.Expensia.Core.Services.Inbox
                                 && (!input.CompanyId.HasValue || input.CompanyId.Equals(otp.CompanyId))
                                 && (!input.TransactionTypeId.HasValue || input.TransactionTypeId.Equals(otp.TransactionTypeId))
                                 && (!input.FromBankAliasId.HasValue || input.FromBankAliasId.Equals(otp.FromBankAliasId))
-								&& (string.IsNullOrEmpty(searchKey) || tt.Description.ToLower().Contains(searchKey) || ibx.TransactionNo.ToLower().Contains(searchKey)
-                                || otp.CompanyName.ToLower().Contains(searchKey) || otp.FromBankAliasName.ToLower().Contains(searchKey)
-                                || otp.ToBankAliasName.ToLower().Contains(searchKey) || otp.TotalAmount.ToString().Contains(searchKey)
-                                || otp.Remark.ToLower().Contains(searchKey) || otp.Requestor.ToLower().Contains(searchKey))
+								&& (
+									string.IsNullOrEmpty(searchKey) 
+									|| tt.Description.ToLower().Contains(searchKey) 
+									|| ibx.TransactionNo.ToLower().Contains(searchKey)
+									|| otp.CompanyName.ToLower().Contains(searchKey) 
+									|| otp.FromBankAliasName.ToLower().Contains(searchKey)
+									|| otp.ToBankAliasName.ToLower().Contains(searchKey) 
+									|| otp.TotalAmount.ToString().Contains(searchKey)
+									|| otp.Remark.ToLower().Contains(searchKey) 
+									|| otp.Requestor.ToLower().Contains(searchKey)
+								)
 								&& ibx.ApprovalStatus == ApprovalStatus.WaitingApproval
                             orderby otp.TotalAmount descending
                             select new ListInboxDto
