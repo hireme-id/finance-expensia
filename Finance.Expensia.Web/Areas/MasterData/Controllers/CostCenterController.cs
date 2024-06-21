6777339413c7088ba69fb582691add8db0fc9283
ronnyharianto <v.ronny.harianto@gmail.com>
2024-06-21 11:23:31 +0700

    feat: add manage customer (cost center)

--- Finance.Expensia.Web/Areas/MasterData/Controllers/CostCenterController.cs
+++ Finance.Expensia.Web/Areas/MasterData/Controllers/CostCenterController.cs
@@ -1,23 +1,63 @@
 ﻿using Finance.Expensia.Core.Services.MasterData;
 using Finance.Expensia.Core.Services.MasterData.Dtos;
+using Finance.Expensia.Core.Services.MasterData.Inputs;
 using Finance.Expensia.Shared.Attributes;
-using Finance.Expensia.Shared.Constants;
+using Finance.Expensia.Shared.Objects;
 using Finance.Expensia.Shared.Objects.Dtos;
+using Finance.Expensia.Shared.Objects.Inputs;
 using Finance.Expensia.Web.Controllers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static Finance.Expensia.Shared.Constants.PermissionConstants.MasterData;
 
 namespace Finance.Expensia.Web.Areas.MasterData.Controllers
 {
-    [Route("[controller]")]
-    public class CostCenterController(CostCenterService costCenterService) : BaseController
+    public class CostCenterController(CostCenterService costCenterService, CurrentUserAccessor currentUserAccessor) : BaseController
     {
         private readonly CostCenterService _costCenterService = costCenterService;
+        private readonly CurrentUserAccessor _currentUserAccessor = currentUserAccessor;
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         [HttpPost("ddlcostcenter")]
-        [AppAuthorize(PermissionConstants.MasterData.CostCenterView)]
+        [AppAuthorize(CostCenter.CostCenterView)]
         public async Task<ResponseObject<List<CostCenterDto>>> RetrieveCostCenter(Guid companyId)
         {
             return await _costCenterService.RetrieveCostCenter(companyId);
         }
-    }
+
+		[HttpPost("costcenter/list")]
+		[AppAuthorize(CostCenter.CostCenterView)]
+		public async Task<ResponsePaging<CostCenterDto>> GetListCostCenter([FromBody] PagingSearchInputBase input)
+		{
+			return await _costCenterService.GetListPartner(input, _currentUserAccessor);
+		}
+
+		[AppAuthorize(CostCenter.CostCenterView)]
+		[HttpPost("costcenter/detail")]
+		public async Task<ResponseObject<CostCenterDto>> GetDetailCostCenter([FromQuery] Guid costCenterId)
+		{
+			return await _costCenterService.GetDetailCostCenter(costCenterId);
+		}
+
+		[Mutation]
+		[AppAuthorize(CostCenter.CostCenterUpsert)]
+		[HttpPost("costcenter/upsert")]
+		public async Task<ResponseBase> UpsertCostCenter([FromBody] UpsertCostCenterInput input)
+		{
+			return await _costCenterService.UpsertCostCenter(input);
+		}
+
+		[Mutation]
+		[AppAuthorize(CostCenter.CostCenterDelete)]
+		[HttpPost("costcenter/delete")]
+		public async Task<ResponseBase> DeleteCostCenter([FromQuery] Guid costCenterId)
+		{
+			return await _costCenterService.DeleteCostCenter(costCenterId);
+		}
+	}
 }
